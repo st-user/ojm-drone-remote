9afe8f396dfa729f0014e886d2e76f34bd653d48
st-user <nck.cd.339@gmail.com>
2021-06-14 13:11:12 +0900

    Make it possible to use without firestore

--- server/components/Storage.js
+++ server/components/Storage.js
@@ -1,25 +1,43 @@
 const {
+    isDevelopment,
     STORAGE_PROJEDT_ID,
     STORAGE_KEY_PATH
 } = require('./Environment.js');
 
 const Firestore = require('@google-cloud/firestore');
 
-module.exports = class Storage {
+const logger = require('./Logger.js');
 
-    constructor() {
-        this._db = new Firestore({
-            projectId: STORAGE_PROJEDT_ID,
-            keyFilename: STORAGE_KEY_PATH,
-        });
-    }
+if (!STORAGE_PROJEDT_ID && isDevelopment) {
 
-    async getAccessTokens() {
-        const snapshot = await this._db.collection('accessKeyHashes').get();
-        const ret = [];
-        snapshot.forEach(doc => {
-            ret.push(doc.data().value);
-        });
-        return ret;
-    }
-};
+    logger.info('STORAGE_PROJEDT_ID is empty and NODE_ENV is development so use static access token.');
+
+    module.exports = class Storage {
+        async getAccessTokens() {
+            return [
+                // key_for_test
+                '5c7f54ceaadad2a8cd17bd862ff05945:77d662c4eb26421d7de51c211cf7574c30a9e412268487e7e50568adcb2e5e7fa0f87e6bf9177dece49b1c21a3d097c479df365b79e1032b54c77525733c774c'
+            ];
+        }
+    };
+} else {
+    module.exports = class Storage {
+
+        constructor() {
+            this._db = new Firestore({
+                projectId: STORAGE_PROJEDT_ID,
+                keyFilename: STORAGE_KEY_PATH,
+            });
+        }
+    
+        async getAccessTokens() {
+            const snapshot = await this._db.collection('accessKeyHashes').get();
+            const ret = [];
+            snapshot.forEach(doc => {
+                ret.push(doc.data().value);
+            });
+            return ret;
+        }
+    };    
+}
+
