0842fb88bcb8495a896d381efa7bdc5ec688b4f8
st-user <nck.cd.339@gmail.com>
2021-07-09 13:15:07 +0900

    Fix function for cleaning up

--- function/cleaner/index.js
+++ function/cleaner/index.js
@@ -19,6 +19,41 @@ const MINUTS_EXPIRES_IN = {
     'remote_events': 1
 };
 
+async function deleteCollection(db, collectionName, batchSize) {
+    const ref = db.collection(collectionName);
+
+    const expiresIn = new Date();
+    const minutesExpiresIn = MINUTS_EXPIRES_IN[collectionName] || 5
+    expiresIn.setMinutes(expiresIn.getMinutes() - minutesExpiresIn);
+    const query = ref.where('timestamp', '<=', expiresIn.getTime()).limit(batchSize);
+
+    return new Promise((resolve, reject) => {
+        deleteQueryBatch(db, query, resolve, collectionName).catch(reject);
+    });
+}
+
+async function deleteQueryBatch(db, query, resolve, collectionName) {
+    const snapshot  =await query.get();
+
+    const batchSize = snapshot.size;
+    console.log(`Delete ${batchSize} documents(s) in ${collectionName}.`);
+
+    if (batchSize === 0) {
+        resolve();
+        return;
+    }
+
+    const batch = db.batch();
+    snapshot.docs.forEach(doc => {
+        batch.delete(doc.ref);
+    });
+    await batch.commit();
+
+    process.nextTick(() => {
+        deleteQueryBatch(db, query, resolve, collectionName);
+    });
+}
+
 exports.cleanup = async (event, context) => {
 
     const db = new Firestore({
@@ -26,25 +61,6 @@ exports.cleanup = async (event, context) => {
     });
 
     for (const collectionName of TAGET_COLLECTIONS) {
-        const colRef = db.collection(collectionName);
-        const expiresIn = new Date();
-        const minutesExpiresIn = MINUTS_EXPIRES_IN[collectionName] || 5
-        expiresIn.setMinutes(expiresIn.getMinutes() - minutesExpiresIn);
-        const query = colRef.where('timestamp', '<=', expiresIn.getTime());
-
-        const snapshot = await query.get();
-        const batchSize = snapshot.size;
-
-        console.log(`Delete ${batchSize} documents(s) in ${collectionName}.`);
-        if (batchSize === 0) {
-            continue;
-        }
-
-        const batch = db.batch();
-        snapshot.docs.forEach(doc => {
-            batch.delete(doc.ref);
-        });
-
-        await batch.commit();
+        await deleteCollection(db, collectionName, 100);
     }
 };
